679631d66ec7ffdaaad3c4c1898f168b9c480f3e
Tron <asfedasov@gmail.com>
2020-09-14 15:01:02 +0300

    FE - update 1.0

---
+++ src/com/dragon/FilterSet.java
@@ -0,0 +1,33 @@
+package src.com.dragon;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class FilterSet {
+
+    public static void main(String[] args) {
+        Set<String> set = new LinkedHashSet<>();
+        removeEvenLength(set);
+    }
+
+    private static void removeEvenLength(Set<String> set) {
+        set.add("foo");
+        set.add("buzz");
+        set.add("bar");
+        set.add("fork");
+        set.add("bart");
+        set.add("spoon");
+        set.add("!");
+        set.add("dude");
+        Iterator<String> iterator = set.iterator();
+        System.out.println("Изначальное множество элементов: " + set);
+        while (iterator.hasNext()) {
+            String temp = iterator.next();
+            if (temp.length() % 2 == 0) {
+                iterator.remove();
+            }
+        }
+        System.out.println("Множество элементов нечетной длины " + set);
+    }
+}
